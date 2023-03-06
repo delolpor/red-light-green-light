ef96572fe58dfca5f64540351d717bde571fc3c2
Diego Elola <diego.elola@unican.es>
2023-03-06 23:33:48 +0100

    feat: add user score component

---
+++ src/components/UserScore.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { useUserStore } from "src/stores/userStore";
+
+const userStore = useUserStore();
+</script>
+<template>
+  <div class="score">
+    <p class="score__user">Player: {{ userStore.playerName }}</p>
+    <p class="score__points">Score: {{ userStore.playerScore }}</p>
+  </div>
+</template>
+<style lang="scss" scoped>
+.score {
+  width: 80vw;
+  background-color: $secondary;
+  border-radius: 5px;
+  margin: auto;
+  padding: 1rem;
+  color: $font-color-light;
+  display: flex;
+  justify-content: space-evenly;
+  box-shadow: 0.25rem 0.25rem;
+
+  &__user,
+  &__points {
+    font-size: 1.5rem;
+  }
+}
+</style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,11 +1,17 @@
 <script setup>
-import router from "src/router";
+import { useUserStore } from "src/stores/userStore";
 import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 const playerName = ref("");
+
+const userStore = useUserStore();
 
 const createPlayer = () => {
   if (playerName.value) {
-    saveUser(playerName.value);
+    userStore.savePlayer(playerName.value);
+
     router.push("/game");
   }
 };
@@ -13,6 +19,18 @@ const createPlayer = () => {
 
 <template>
   <h1>Create new player</h1>
-  <input v-model="playerName" type="text" />
-  <button @click="createPlayer">JOIN</button>
+  <form class="form" @submit.prevent="createPlayer">
+    <q-input v-model="playerName" label="Introduce your name" />
+    <q-btn @click="createPlayer" color="primary" label="JOIN" />
+  </form>
 </template>
+
+<style lang="scss" scoped>
+.form {
+  display: flex;
+  flex-flow: column;
+  justify-content: space-evenly;
+  width: 90%;
+  margin: auto;
+}
+</style>
