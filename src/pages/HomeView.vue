a6de3f12fde79d1b6c3aadea1f689c18da508fe9
Diego Elola <elolad@unican.es>
2023-03-06 15:25:35 +0100

    feat: add basic user store

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,7 +1,18 @@
-<script setup></script>
+<script setup>
+import router from "src/router";
+import { ref } from "vue";
+const playerName = ref("");
+
+const createPlayer = () => {
+  if (playerName.value) {
+    saveUser(playerName.value);
+    router.push("/game");
+  }
+};
+</script>
 
 <template>
   <h1>Create new player</h1>
-  <input type="text" />
-  <button>JOIN</button>
+  <input v-model="playerName" type="text" />
+  <button @click="createPlayer">JOIN</button>
 </template>
